ca02f1eb2a3ed85011b6906422452f6bfb57eb0e
MikiTran <mikitran721@gmail.com>
2023-10-19 01:35:17 +0700

    print & basic variables

---
+++ printOut.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 8.1 Basic</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        echo "Tu dong format code"
+        ?>
+    </h1>
+    <h2><?= 'Chuoi text' ?></h2>
+</body>
+
+</html>
+
+<?php
+echo "hello";
+print "hello";
+print_r(['Poo', 'Phoong']);
+var_dump('Hello');
+var_export('Chao');
---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php
+// echo "VARIABLES: <br>";
+// $name = 'Poo';
+// echo $name;
+// $has_dog = 'Poo';
+// echo $has_dog;
+// $has_cat = true;
+// echo $has_cat; //1
+$has_car = false;
+var_dump($has_car);
+var_export($has_car);
